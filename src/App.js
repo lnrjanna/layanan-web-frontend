e59866bb5ae7de59631300b56cd617d02be1b237
lia <2203010169@unper.ac.id>
2025-05-20 05:53:06 +0700

    feat: replace Login component with Dashboard and update global styles
    Add a new Dashboard component to replace the Login component in the App. The Dashboard includes a responsive layout with Material-UI components and displays statistics such as total products, orders, and visitors. Additionally, update global styles in App.css to ensure full-width and remove default margins and padding for a cleaner layout.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Dashboard />
     </div>
   );
 }
